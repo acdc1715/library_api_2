58f4135606a9ad80e49caa1dff02fe8a307d130b
Volodymyr Avramets <48892109+acdc1715@users.noreply.github.com>
2025-02-21 16:59:30 +0200

    fix warning

--- LibraryAPI/LibraryAPI/Models/Domain/Book.cs
+++ LibraryAPI/LibraryAPI/Models/Domain/Book.cs
@@ -3,15 +3,15 @@
     public class Book
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public string? Description { get; set; }
 
-        public string ContentUrl { get; set; }
+        public required string ContentUrl { get; set; }
 
         public Guid AuthorId { get; set; }
 
 
         //Navigation properties
-        public Author Author { get; set; }
+        public required Author Author { get; set; }
     }
 }
