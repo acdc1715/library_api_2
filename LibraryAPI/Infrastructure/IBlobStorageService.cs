b1ea5a1737e36b12111bd313bd09bdf211cef7ec
Volodymyr Avramets <48892109+acdc1715@users.noreply.github.com>
2025-03-03 13:08:57 +0200

    Split into projects: add Infrastructure

---
+++ LibraryAPI/Infrastructure/IBlobStorageService.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace LibraryAPI.Data
+{
+    public interface IBlobStorageService
+    {
+        Task<string> UploadBlobAsync(IFormFile formFile);
+        Task<bool> DeleteBlobAsync(string fileUrl);
+    }
+}
---
+++ LibraryAPI/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAPI.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryAPI.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IBlobStorageService, BlobStorageService>();
+            return services;
+        }
+    }
+}
