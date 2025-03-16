cfa831dda9e3487c588d7c30d8028f68e39eac6a
Volodymyr Avramets <48892109+acdc1715@users.noreply.github.com>
2025-03-16 19:18:31 +0200

    add EF migrations to git

---
+++ LibraryAPI/DataAccess/Migrations/20241102131721_Seed Genre table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGenretable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("0de90f1d-5cb3-4837-a16d-67c8b126a523"), "Detective" },
+                    { new Guid("563d3547-53dc-49e4-af19-171cd010dfc5"), "Fantasy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: new Guid("0de90f1d-5cb3-4837-a16d-67c8b126a523"));
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: new Guid("563d3547-53dc-49e4-af19-171cd010dfc5"));
+        }
+    }
+}
